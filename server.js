eb97daaa2f53e864bff9da2e4f15a55abae136aa
devartist <kevinwasonga415@outlook.com>
2023-01-29 17:30:25 +0300

    iny

---
+++ server.js
@@ -0,0 +1,12 @@
+//Server infomartion setup
+//app set up
+const dotenv = require("dotenv")
+const app = require("./app")
+
+dotenv.config({path: "./config.env"})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+ console.log(`App running on port ${port}...`)
+})
